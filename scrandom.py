c63bdd5baf512d19cf60a55febf6d5b9a8ecad6f
Alton Caylor <ctrlalton@gmail.com>
2024-03-06 17:09:42 -0500

    Fixed wrong path source

--- scrandom.py
+++ scrandom.py
@@ -9,10 +9,7 @@ import re
 import random
 
 if getattr(sys, 'frozen', False):
-    # If the application is run as a bundle, the PyInstaller bootloader
-    # extends the sys module by a flag frozen=True and sets the app 
-    # path into variable _MEIPASS'.
-    application_path = sys._MEIPASS
+    application_path = os.path.dirname(sys.executable)
 else:
     application_path = os.path.dirname(os.path.abspath(__file__))
 application_path = Path(application_path)
