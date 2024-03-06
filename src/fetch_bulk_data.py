da14613dbe9249b6f12c396ac587eb056579109e
Alton Caylor <ctrlalton@gmail.com>
2024-03-06 09:43:39 -0500

    Process tweaking

--- src/fetch_bulk_data.py
+++ src/fetch_bulk_data.py
@@ -3,6 +3,7 @@ import json
 import os
 from datetime import datetime
 from pathlib import Path
+import time
 
 TYPE = "oracle-cards"
 CWD = Path.cwd()
@@ -21,7 +22,7 @@ def get_download_uri():
 
 
 def get_data(uri):
-    print(f"Downloading file at '{uri}'...")
+    print(f"Retrieving data from '{uri}'...")
     x = requests.get(uri).json()
     print(f"Successfully downloaded file at '{uri}'.")
     return x
@@ -35,14 +36,17 @@ def write_to_json(file, name):
     print(f"Successfully saved file to '{filepath}'.")
 
 
-def validate_directory(filename):
+def ensure_dir_exists():
     if not os.path.isdir(DIRECTORY):
-        os.mkdir(DIRECTORY)
+        os.makedirs(DIRECTORY)
+
+def clear_directory(filename, force=False):
+    ensure_dir_exists()
     with os.scandir(DIRECTORY) as it:
         for entry in it:
             if entry.name.split("_")[0] != filename.split("_")[0]:
                 continue
-            if entry.name != filename:
+            if entry.name != filename or force:
                 os.remove(entry)
 
 
@@ -52,40 +56,42 @@ def paginate(uri):
     all_data = []
 
     while True:
-        all_data += [i["name"] for i in file["data"]]
+        all_data += [i for i in file["data"]]
         if not "next_page" in dict.keys(file):
             break
+        time.sleep(0.1)
         file = get_data(file["next_page"])
     assert total == len(all_data)
 
     return all_data
 
 
-def get_data_if_none_exists(filename, uri, func):
-    if filename not in [i.name for i in os.scandir(DIRECTORY)]:
-        data = func(uri)
-        write_to_json(data, filename)
-    else:
+def get_data_if_none_exists(filename, uri, func, force=False):
+    if filename in [i.name for i in os.scandir(DIRECTORY)] and force is False:
         print("Up-to-date data already exists. Download was skipped.")
+        return 0
+    data = func(uri)
+    write_to_json(data, filename)
+    return 1
 
 
-def fetch():
+def fetch(force=False):
     uri = get_download_uri()
     filename = f"oracle-cards_{datetime.today().strftime('%Y%m%d')}.json"
-    validate_directory(filename)
-    get_data_if_none_exists(filename, uri, get_data)
+    clear_directory(filename, force)
+    get_data_if_none_exists(filename, uri, get_data, force)
 
 
-def fetch_all_commanders():
-    uri = "https://api.scryfall.com/cards/search?q=is%3Acommander"
+def fetch_all_commanders(force=False):
+    uri = "https://api.scryfall.com/cards/search?q=is%3Acommander+legal%3Acommander"
     filename = f"all-commanders_{datetime.today().strftime('%Y%m%d')}.json"
-    validate_directory(filename)
-    get_data_if_none_exists(filename, uri, paginate)
+    clear_directory(filename, force)
+    get_data_if_none_exists(filename, uri, paginate, force)
 
 
 def main():
     fetch()
-    fetch_all_commanders()
+    fetch_all_commanders(force=True)
 
 
 if __name__ == "__main__":
